b20af96bbe5f32dd4bc4442e7659813403f67cf2
Ivan Krivosheja <ranger3331@gmail.com>
2016-03-09 18:24:48 +0200

    add the first JS Homework

---
+++ www/JSHomework2/js/script.js
@@ -0,0 +1,17 @@
+﻿var arrOfName = [];
+
+for (var i = 0; i < 5; i++) {
+    arrOfName[i] = prompt('Введите имя', '');
+}
+var login = prompt ('Введите имя пользователя');
+
+
+for (var i = 0; i < arrOfName[i].length; i++ ) {
+    if ( login == arrOfName[i] ) {
+        alert ('Вы успешно вошли')
+    } else {
+        alert ( 'Ошибка входа' )
+    }
+}
+
+
